38816eae6fba70d0d5f4236cffc23cc08b750229
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-03-09 16:46:27 +0530

    Update tori_index.js

--- tori_index.js
+++ tori_index.js
@@ -35,8 +35,8 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 		const data = await rest.put(
-			 Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), 
-             		//Routes.applicationCommands(process.env.CLIENT_ID),
+			    // Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), 
+             	Routes.applicationCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
